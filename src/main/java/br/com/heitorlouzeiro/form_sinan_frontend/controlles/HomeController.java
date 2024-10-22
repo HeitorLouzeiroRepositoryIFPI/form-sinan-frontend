a50d135d0e439e795777e7a2241557c92d122fe4
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-10-21 22:19:04 -0300

    front end do site com springboot

--- src/main/java/br/com/heitorlouzeiro/form_sinan_frontend/controlles/HomeController.java
+++ src/main/java/br/com/heitorlouzeiro/form_sinan_frontend/controlles/HomeController.java
@@ -1,10 +1,15 @@
+
 package br.com.heitorlouzeiro.form_sinan_frontend.controlles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(Model model) {
+        model.addAttribute("tituloPagina", "Página Inicial");
+        return "pages/homePage";
     }
-    
 }
