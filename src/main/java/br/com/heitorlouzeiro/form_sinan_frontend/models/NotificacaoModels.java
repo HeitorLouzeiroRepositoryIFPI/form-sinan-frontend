6b10de9c20fca8efc3b3a4a42fa2c5a525e99537
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-10-21 23:02:48 -0300

    Adicionado NotificacaoModels class

---
+++ src/main/java/br/com/heitorlouzeiro/form_sinan_frontend/models/NotificacaoModels.java
@@ -0,0 +1,234 @@
+package br.com.heitorlouzeiro.form_sinan_frontend.models;
+
+import java.time.LocalDate;
+
+public class NotificacaoModels {
+    private String tipoNotificacao;
+    private Integer agravoDoenca;
+    private LocalDate dataNotificacao;
+    private String uf;
+    private String municipioNotificacao;
+    private String unidadeSaude;
+    private String codigoUnidade;
+    private LocalDate dataPrimeirosSintomas;
+    private String nomePaciente;
+    private LocalDate dataNascimento;
+    private Integer idade;
+    private String tipoIdade;
+    private String sexo;
+    private Integer gestante;
+    private Integer racaCor;
+    private Integer escolaridade;
+    private String cartaoSus;
+    private String nomeMae;
+    private String ufResidencia;
+    private String municipioResidencia;
+    private String distrito;
+    private String bairro;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String geoCampo1;
+    private String geoCampo2;
+    private String pontoReferencia;
+    private String cep;
+    private String telefone;
+    private Integer zona;
+    private String pais;
+
+    public NotificacaoModels() {
+        
+    }
+    public String getTipoNotificacao() {
+        return tipoNotificacao;
+    }
+    public void setTipoNotificacao(String tipoNotificacao) {
+        this.tipoNotificacao = tipoNotificacao;
+    }
+    public Integer getAgravoDoenca() {
+        return agravoDoenca;
+    }
+    public void setAgravoDoenca(Integer agravoDoenca) {
+        this.agravoDoenca = agravoDoenca;
+    }
+    public LocalDate getDataNotificacao() {
+        return dataNotificacao;
+    }
+    public void setDataNotificacao(LocalDate dataNotificacao) {
+        this.dataNotificacao = dataNotificacao;
+    }
+    public String getUf() {
+        return uf;
+    }
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    public String getMunicipioNotificacao() {
+        return municipioNotificacao;
+    }
+    public void setMunicipioNotificacao(String municipioNotificacao) {
+        this.municipioNotificacao = municipioNotificacao;
+    }
+    public String getUnidadeSaude() {
+        return unidadeSaude;
+    }
+    public void setUnidadeSaude(String unidadeSaude) {
+        this.unidadeSaude = unidadeSaude;
+    }
+    public String getCodigoUnidade() {
+        return codigoUnidade;
+    }
+    public void setCodigoUnidade(String codigoUnidade) {
+        this.codigoUnidade = codigoUnidade;
+    }
+    public LocalDate getDataPrimeirosSintomas() {
+        return dataPrimeirosSintomas;
+    }
+    public void setDataPrimeirosSintomas(LocalDate dataPrimeirosSintomas) {
+        this.dataPrimeirosSintomas = dataPrimeirosSintomas;
+    }
+    public String getNomePaciente() {
+        return nomePaciente;
+    }
+    public void setNomePaciente(String nomePaciente) {
+        this.nomePaciente = nomePaciente;
+    }
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+    public String getTipoIdade() {
+        return tipoIdade;
+    }
+    public void setTipoIdade(String tipoIdade) {
+        this.tipoIdade = tipoIdade;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public Integer getGestante() {
+        return gestante;
+    }
+    public void setGestante(Integer gestante) {
+        this.gestante = gestante;
+    }
+    public Integer getRacaCor() {
+        return racaCor;
+    }
+    public void setRacaCor(Integer racaCor) {
+        this.racaCor = racaCor;
+    }
+    public Integer getEscolaridade() {
+        return escolaridade;
+    }
+    public void setEscolaridade(Integer escolaridade) {
+        this.escolaridade = escolaridade;
+    }
+    public String getCartaoSus() {
+        return cartaoSus;
+    }
+    public void setCartaoSus(String cartaoSus) {
+        this.cartaoSus = cartaoSus;
+    }
+    public String getNomeMae() {
+        return nomeMae;
+    }
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+    public String getUfResidencia() {
+        return ufResidencia;
+    }
+    public void setUfResidencia(String ufResidencia) {
+        this.ufResidencia = ufResidencia;
+    }
+    public String getMunicipioResidencia() {
+        return municipioResidencia;
+    }
+    public void setMunicipioResidencia(String municipioResidencia) {
+        this.municipioResidencia = municipioResidencia;
+    }
+    public String getDistrito() {
+        return distrito;
+    }
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    public String getGeoCampo1() {
+        return geoCampo1;
+    }
+    public void setGeoCampo1(String geoCampo1) {
+        this.geoCampo1 = geoCampo1;
+    }
+    public String getGeoCampo2() {
+        return geoCampo2;
+    }
+    public void setGeoCampo2(String geoCampo2) {
+        this.geoCampo2 = geoCampo2;
+    }
+    public String getPontoReferencia() {
+        return pontoReferencia;
+    }
+    public void setPontoReferencia(String pontoReferencia) {
+        this.pontoReferencia = pontoReferencia;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public Integer getZona() {
+        return zona;
+    }
+    public void setZona(Integer zona) {
+        this.zona = zona;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
